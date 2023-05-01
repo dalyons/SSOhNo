ff6641c1594e1d61b23db71911bc36e1a0c96ea9
Dave Lyons <dlyons@chime.com>
2023-05-01 16:54:32 -0700

    scrach ui

--- options.js
+++ options.js
@@ -1,11 +1,21 @@
 document.getElementById('saveButton').addEventListener('click', () => {
-  const buttonSelectors = document.getElementById('buttonSelectors').value.split(',').map(selector => selector.trim());
-  const closeUrls = document.getElementById('closeUrls').value.split(',').map(url => url.trim());
+  //const buttonSelectors = document.getElementById('buttonSelectors').value.split(',').map(selector => selector.trim());
+  //const closeUrls = document.getElementById('closeUrls').value.split(',').map(url => url.trim());
+
+  var closeUrls = Array.from(document.querySelectorAll('.tab-closers input[type="checkbox"]:checked')).map(
+    elm => elm.value
+  );
+  const closeExtra = document.getElementById('additionalMatchersClosers').value;
+  if (closeExtra != "") closeUrls.push(closeExtra);
+
+
+alert(closeUrls)
+
   chrome.storage.sync.set({ buttonSelectors, closeUrls }, () => {
     alert('Button selectors and close URLs saved.');
   });
 });
-
+/*
 chrome.storage.sync.get(['buttonSelectors', 'closeUrls'], ({ buttonSelectors, closeUrls }) => {
   if (buttonSelectors) {
     document.getElementById('buttonSelectors').value = buttonSelectors.join(', ');
@@ -14,4 +24,4 @@ chrome.storage.sync.get(['buttonSelectors', 'closeUrls'], ({ buttonSelectors, cl
     document.getElementById('closeUrls').value = closeUrls.join(', ');
   }
 });
-
+*/
